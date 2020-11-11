c2201e81800fdcf2c893745b9bf8572a34a52906
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-11 23:55:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,9 +110,9 @@ def changing_alan
       }
     }
   programmer_hash[:alan_kay][:known_for] = "GUI"
+  
   programmer_hash
-  
-  
+
 end
 
 def adding_to_dennis
@@ -134,5 +134,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  
+  programmer_hash
 end

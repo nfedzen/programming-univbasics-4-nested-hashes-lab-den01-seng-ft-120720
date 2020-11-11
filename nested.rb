a2ce740401f245e47ac3d6300cb463702e8e0489
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-11 23:56:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,13 +81,19 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    
+    programmer_hash
 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-
+  
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -103,6 +109,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 
 end
 
@@ -125,5 +134,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  
+  programmer_hash
 end
